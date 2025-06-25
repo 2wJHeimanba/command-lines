45b59903f81f34235e28335e9c2f5096a0fbe808
2wj宁静致远 <zwj18506698903@163.com>
2025-06-25 23:54:37 +0800

    ...

--- examples/test.rs
+++ examples/test.rs
@@ -1,13 +1,48 @@
-struct Age(u8);
-impl TryFrom<i32> for Age {
-  type Error = &'static str;
-  fn try_from(value: i32) -> Result<Self, Self::Error> {
-    Ok(Self(value as u8))
+use std::fmt::Display;
+
+trait People {
+  type Gender: Display;
+  // type Age = i32;
+  const LANGUAGE: &'static str = "wenjian";
+  const NAME: i32;
+
+  fn new() -> Self;
+  fn get_name(&self);
+
+  fn is_people() -> bool {
+    Self::LANGUAGE;
+    true
+  }
+}
+
+trait People2 {
+  fn is_people() -> bool {
+    false
+  }
+}
+// const name: String = String::from("fdsafds");
+struct Person;
+impl People2 for Person {}
+impl People for Person {
+  const NAME: i32 = 23;
+  type Gender = String;
+  fn new() -> Self {
+    Person
+  }
+  fn get_name(&self) {
+    println!("person");
   }
 }
 
 fn main() {
-  let age: Age = 23i32.try_into().unwrap();
-  let content = age.0;
-  println!("content: {}", content);
+  // let test = People::is_people();
+  let person = Person;
+  let res = Person::new();
+  let bo = <Person as People>::is_people();
+  if bo {
+    println!("是真的");
+  } else {
+    println!("不是真的");
+  }
+  println!("fdsafds");
 }
